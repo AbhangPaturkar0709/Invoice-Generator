c8bab4bb67f5ebe770c80131fab01934ffa64e52
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2025-08-22 14:49:46 +0530

    error solved

--- server/server.js
+++ server/server.js
@@ -10,36 +10,18 @@ connectDB();
 
 const app = express();
 app.use(json());
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://invoice-generator-8lfq.onrender.com"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (e.g. mobile apps, curl, Postman)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bills", billRoutes);
 
 // Example Protected Route
-import { authMiddleware } from "./middleware/authMiddleware.js";
+import {authMiddleware} from "./middleware/authMiddleware.js";
 app.get("/api/profile", authMiddleware, (req, res) => {
-  res.json({ msg: "Welcome to your profile", user: req.user });
+  res.json({ msg: "Welcome to your profile", user: req.
+    user });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
